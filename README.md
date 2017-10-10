627fab1cb42eb31d1eee8aa354041fcd94c64640
Dawson Mortenson and Riki Montgomery <riki@>
2017-10-10 09:16:39 -0700

    add quick setup instructions for javascript projects

---
+++ README.md
@@ -0,0 +1,54 @@
+# Javascript Track Project Setup
+___
+### Project Files
+`touch index.html .gitignore README.md gulpfile.js js/NAME_OF_PROJECT.js js/NAME_OF_PROJECT-INTERFACE.js`
+___
+## npm
+1. `npm init`
+1. `npm install gulp --save-dev` (global install: `npm install gulp -g`)
+1. `npm install bower -g`
+1. `npm install browserify --save-dev`
+1. `npm install vinyl-source-stream --save-dev`
+1. `npm install gulp-concat --save-dev`
+1. `npm install gulp-uglify --save-dev`
+1. `npm install gulp-util --save-dev`
+1. `npm install del --save-dev`
+1. `npm install jshint --save-dev`
+1. `npm install gulp-jshint --save-dev`
+1. `npm install bower-files --save-dev`
+1. `npm install browser-sync --save-dev`
+1. `npm install watchify --save-dev`
+1. `npm install karma --save-dev`
+1. `npm install karma-jasmine jasmine-core --save-dev`
+1. `npm install karma-chrome-launcher --save-dev`
+1. `npm install karma-cli --save-dev`
+1. `npm install karma-browserify --save-dev`
+1. `npm install karma-jquery --save-dev`
+1. `npm install karma-jasmine-html-reporter --save-dev`
+
+#### OR...Master npm Setup Command (Must run `npm init` first)
+_`npm install gulp bower browserify vinyl-source-stream gulp-concat gulp-uglify gulp-util del jshint gulp-jshint bower-files browser-sync watchify karma karma-jasmine jasmine-core karma-chrome-launcher karma-cli karma-browserify karma-jquery karma-jasmine-html-reporter --save-dev`_
+___
+### bower
+* `bower init`
+* `bower install jquery --save`
+* `bower install bootstrap#v4.0.0 --save`
+* `bower install moment --save`
+
+#### OR...Master npm Setup Command (Must run `bower init` first)
+_`bower install jquery bootstrap#v4.0.0-beta moment --save`_
+___
+# Javascript Track Project Clone
+* `npm init`
+* `npm install`
+* `npm install bower -g`
+* `bower init`
+* `bower install`
+* `gulp build`
+___
+### karma --optional
+_Use template "karma.conf.js"_
+
+_OR_
+
+_`karma init` in root proj directory_
---
+++ gulp.js
@@ -0,0 +1,96 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create();
+
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+
+var buildProduction = utilities.env.production;
+
+// dependencies
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/pingpong-interface.js', './js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+  gulp.start('bower');
+});
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
